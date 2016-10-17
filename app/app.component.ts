8f4d2bd43e571eab9661f7c58c4f9ef4ba02b160
Katrina Hirsch and Shelby Clayton <katrina@>
2016-10-17 09:25:08 -0700

    create initial project files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'to-do',
+  template: `
+  <div class="container">
+    <h1>To-Do:</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
