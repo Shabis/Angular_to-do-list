be0c6d6cf5673b76595fdaf5e3fad743fe709cd6
Katrina Hirsch and Shelby Clayton <shelby@>
2016-10-17 15:48:32 -0700

    do a bunch of stuff

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+  ],
+  bootstrap:    [ AppComponent ]
 })
 
 export class AppModule { }
